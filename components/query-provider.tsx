69db32ceccefdc95e050a4ffe656dfa8a432d879
Moksh Keloo <moksh@integratedwebworks.com>
2024-12-28 19:18:09 -0500

    Use & install React Query from Tanstack.

---
+++ components/query-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function QueryProvider({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+          },
+        },
+      })
+  );
+
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </div>
+  );
+}
